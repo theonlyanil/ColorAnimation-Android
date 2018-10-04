0629673a7759fe3ab7b2d56f6749c8cf3b3c1de1
Anil Sardiwal <anil@widefide.com>
2018-10-04 22:38:50 +0530

    0.2.2 working

--- app/src/main/java/anil/sardiwal/coloranimation/MainActivity.java
+++ app/src/main/java/anil/sardiwal/coloranimation/MainActivity.java
@@ -1,19 +1,98 @@
 package anil.sardiwal.coloranimation;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 import anil.sardiwal.coloranimate.ColorAnimation;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int pos = 0;
+    private RelativeLayout relativeLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RelativeLayout relativeLayout = findViewById(R.id.relativelayout);
+        relativeLayout = findViewById(R.id.relativelayout);
 
-        ColorAnimation.rgb(relativeLayout, 6000);
+        ColorAnimation.rgb(relativeLayout, 2000, this);
+        //ColorAnimation(relativeLayout);
+    }
+
+    private void ColorAnimation(final ViewGroup viewGroup)
+    {
+        final Handler handler = new Handler();
+        Runnable runnable;
+        // It's ON
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                int colorFrom = ((ColorDrawable) viewGroup.getBackground()).getColor();
+                int colorTo = returningColor();
+                int time = 2000; //4000;
+
+                colorAnimation(colorFrom, colorTo, viewGroup, time);
+                handler.postDelayed(this, time/4);
+            }
+        };
+
+        handler.postDelayed(runnable, 100);
+
+    }
+
+    public void colorAnimation(int colorFrom, int colorTo, final View view, int animDuration)
+    {
+        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+        colorAnimation.setDuration(animDuration); // milliseconds
+        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                if(view != null)
+                {
+                    view.setBackgroundColor((int) animator.getAnimatedValue());
+                }
+            }
+
+        });
+        colorAnimation.start();
+    }
+
+    /*
+        Color Animation Code STARTS
+     */
+    // Continuous Color change - Action bar / Toolbar
+    private int returningColor()
+    {
+        final int[] rainbowColors = new int[]
+                {
+
+                        getResources().getColor(R.color.red),
+                        getResources().getColor(R.color.green),
+                        getResources().getColor(R.color.blue),
+                        getResources().getColor(R.color.green),
+                        getResources().getColor(R.color.red)
+
+                };
+
+
+        if(pos < rainbowColors.length-1)
+        {
+            pos++;
+            return rainbowColors[pos];
+        }
+        else
+        {
+            pos = 0;
+            return rainbowColors[pos];
+        }
     }
 }
--- coloranimate/src/main/java/anil/sardiwal/coloranimate/ColorAnimation.java
+++ coloranimate/src/main/java/anil/sardiwal/coloranimate/ColorAnimation.java
@@ -2,41 +2,39 @@ package anil.sardiwal.coloranimate;
 
 import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
+import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class ColorAnimation
 {
     public static int pos = 0;
+    public static Context context;
 
-    public static void rgb(final Handler handler, final ViewGroup view, final int time)
+    public static void rgb(final ViewGroup view, final int time, Context context)
     {
+        ColorAnimation.context = context;
+        final Handler handler = new Handler();
+        Runnable runnable;
         // It's ON
-        Runnable runnable = new Runnable() {
+        runnable = new Runnable() {
             @Override
             public void run() {
-                try
-                {
-                    int colorFrom = ((ColorDrawable) view.getBackground()).getColor();
-                    int colorTo = RGB.returningColor();
+                int colorFrom = ((ColorDrawable) view.getBackground()).getColor();
+                int colorTo = RGB.returningColor();
+                int time = 2000; //4000;
 
-                    animate(colorFrom, colorTo, view, time);
-                }
-                catch (Exception e)
-                {
-                    Log.e("ColorAnimation.Anil", e.getLocalizedMessage());
-                }
-                handler.postDelayed(this, time / 4);
+                colorAnimation(colorFrom, colorTo, view, time);
+                handler.postDelayed(this, time/4);
             }
         };
 
         handler.postDelayed(runnable, 100);
     }
-    
-    private static void animate(int colorFrom, int colorTo, final View view, int animDuration)
+
+    public static void colorAnimation(int colorFrom, int colorTo, final View view, int animDuration)
     {
         ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
         colorAnimation.setDuration(animDuration); // milliseconds
@@ -54,5 +52,59 @@ public class ColorAnimation
         colorAnimation.start();
     }
 
+    public static class RGB
+    {
+        // Continuous Color change - Action bar / Toolbar
+        public static int returningColor()
+        {
+            final int[] rainbowColors = new int[]
+                    {
+
+                            context.getResources().getColor(R.color.red),
+                            context.getResources().getColor(R.color.pink),
+                            context.getResources().getColor(R.color.purple),
+                            context.getResources().getColor(R.color.deep_purple),
+                            context.getResources().getColor(R.color.indigo),
+                            context.getResources().getColor(R.color.blue),
+                            context.getResources().getColor(R.color.light_blue),
+                            context.getResources().getColor(R.color.cyan),
+                            context.getResources().getColor(R.color.teal),
+                            context.getResources().getColor(R.color.green),
+                            context.getResources().getColor(R.color.light_green),
+                            context.getResources().getColor(R.color.lime),
+                            context.getResources().getColor(R.color.yellow),
+                            context.getResources().getColor(R.color.amber),
+                            context.getResources().getColor(R.color.orange),
+                            context.getResources().getColor(R.color.deep_orange),
+                            context.getResources().getColor(R.color.orange),
+                            context.getResources().getColor(R.color.amber),
+                            context.getResources().getColor(R.color.yellow),
+                            context.getResources().getColor(R.color.lime),
+                            context.getResources().getColor(R.color.light_green),
+                            context.getResources().getColor(R.color.green),
+                            context.getResources().getColor(R.color.teal),
+                            context.getResources().getColor(R.color.cyan),
+                            context.getResources().getColor(R.color.light_blue),
+                            context.getResources().getColor(R.color.blue),
+                            context.getResources().getColor(R.color.indigo),
+                            context.getResources().getColor(R.color.deep_purple),
+                            context.getResources().getColor(R.color.purple),
+                            context.getResources().getColor(R.color.pink),
+                            context.getResources().getColor(R.color.red)
+                    };
+
+
+            if(pos < rainbowColors.length-1)
+            {
+                pos++;
+                return rainbowColors[pos];
+            }
+            else
+            {
+                pos = 0;
+                return rainbowColors[pos];
+            }
+        }
+    }
 
 }
